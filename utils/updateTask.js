0a329655287290737c7c0035f1968c250fef46b6
Hassan <hassanhauda@gmail.com>
2024-05-29 02:37:01 +0100

    refactor: Remove console log from updateTask.js

--- utils/updateTask.js
+++ utils/updateTask.js
@@ -23,7 +23,6 @@ const checkAndUpdateTaskStatus = async (scheduledSms, model) => {
 	}
 };
 
-console.log('Update Task Module Loaded');
 
 // Set all tasks to their actual status when the server restarts
 const startup = async () => {
